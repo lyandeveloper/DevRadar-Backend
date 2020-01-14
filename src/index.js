9c0d2b42352a33fbc9a7f1c5e33dd830a57b7437
lyandeveloper <tutors3d@gmail.com>
2020-01-14 10:54:44 -0300

    MongoDB connection sucessfull

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+mongoose.connect(
+  'mongodb+srv://omnistack:admin@cluster0-3vuws.mongodb.net/week10?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const app = express();
 
